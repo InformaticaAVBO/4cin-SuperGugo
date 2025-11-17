544d15ca78f2c2b190a5b63777f087738ad91327
SuperGugo <supergugo@protonmail.com>
2025-11-17 16:53:25 +0000

    umadacchi

--- ADT/src/App.java
+++ ADT/src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Vettore<String> x = new Vettore();
+        Stack<String> x = new Stack();
         for (int i=0; i<50; i++)
             x.push("benni " + i);
         for (int i=0; i<50; i++)
-            System.out.println(x.get(i));
+            System.out.println(x.pop(i));
     }
 }
